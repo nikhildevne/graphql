4ac4eed51cce6d4c370c3aaedb0501abc101588c
nikhildevne <nikhildevne@yahoo.com>
2024-10-16 15:08:24 +0530

    changes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Route to serve index.html
 app.get('/', (req, res) => {
-    res.send('Hello World from AWS Elastic Beanstalk!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
