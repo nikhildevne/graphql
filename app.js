6716b9823f842e6867603bbc0167a9cfc7629fea
nikhildevne <nikhildevne@yahoo.com>
2024-10-16 16:32:14 +0530

    new changes

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ const app = express();
 const port = 3000;
 
 // Set up Handlebars as the view engine
+console.log('enters in app.js')
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 // Define a route to render the Handlebars template
-app.get('/', (req, res) => {
+app.get('/profile', (req, res) => {
+    console.log('comes here...')
     res.render('home', { title: 'My Handlebars App', message: 'Welcome to Handlebars!' });
 });
+
+app.get('/',(req,res)=>{
+    res.send('Hello Nikhil Devne')
+})
 
 // Start the server
 app.listen(port, () => {
