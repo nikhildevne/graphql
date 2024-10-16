1671c4a61362fc6056cda19243ca848a84a652f8
nikhildevne <nikhildevne@yahoo.com>
2024-10-16 20:20:33 +0530

    hippen change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3002
 const templ = `<html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -58,7 +58,7 @@ const templ = `<html lang="en">
     <h1>Resume - Nikhil Nandkumar Devne</h1>
     <div class="contact-info">
         <p>Email: nikhildevne@yahoo.com</p>
-        <p>Phone: 9175669865</p>
+        <p>Phone: +91 9175669865</p>
         <p>Address: Baner, Pune Maharashtra</p>
     </div>
 
